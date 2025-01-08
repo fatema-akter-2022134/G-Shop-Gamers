a95c29b1ba7e6dfdea45860afd430b3beb3d3331
Fatema Akter <2022134@iub.edu.bd>
2025-01-08 14:48:21 +0600

    Add files via upload
    [Major]: Asset uploaded and major bug fixed

---
+++ assets/js/scriptTD.js
@@ -0,0 +1,43 @@
+
+
+// Swipe Function
+var swiper = new Swiper(".trending-content", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    1068: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
+
+// ScrollBar OnScroll Function
+// window.onscroll = function () {
+//   mufunction();
+// };
+
+// function mufunction() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height =
+//     document.documentElement.scrollHeight -
+//     document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+//   document.getElementById("scroll-bar").style.width = scrolled + "%";
+// }
